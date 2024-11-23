c79c5692874616c9871eb4c189dee0b78e6ee349
Ayushi-Sharma19 <ayushiisha19@gmail.com>
2024-11-23 13:06:34 +0530

    Update p10.cpp

--- p10.cpp
+++ p10.cpp
@@ -1,59 +1,63 @@
 #include <iostream>
 using namespace std;
-
-// Forward declaration of classes
-class A;
 class B;
 class C;
-
-// Friend function declaration
-void displayData(A&, B&, C&);
 
 class A {
 private:
     int privateA;
+
 protected:
     int protectedA;
+
 public:
-    A(int pA, int prA) : privateA(pA), protectedA(prA) {}
-    friend void displayData(A&, B&, C&);
+    A(int a, int b) : privateA(a), protectedA(b) {}
+    friend void accessData(A, B, C);
 };
 
 class B {
 private:
     int privateB;
+
 protected:
     int protectedB;
+
 public:
-    B(int pB, int prB) : privateB(pB), protectedB(prB) {}
-    friend void displayData(A&, B&, C&);
+    B(int b, int c) : privateB(b), protectedB(c) {}
+    friend void accessData(A, B, C);
 };
 
 class C {
 private:
     int privateC;
+
 protected:
     int protectedC;
+
 public:
-    C(int pC, int prC) : privateC(pC), protectedC(prC) {}
-    friend void displayData(A&, B&, C&);
+    C(int c, int d) : privateC(c), protectedC(d) {}
+    friend void accessData(A, B, C);
 };
 
-// Friend function definition
-void displayData(A& objA, B& objB, C& objC) {
-    cout << "Class A: Private = " << objA.privateA << ", Protected = " << objA.protectedA << endl;
-    cout << "Class B: Private = " << objB.privateB << ", Protected = " << objB.protectedB << endl;
-    cout << "Class C: Private = " << objC.privateC << ", Protected = " << objC.protectedC << endl;
+void accessData(A a, B b, C c) {
+    cout << "Accessing data from class A:" << endl;
+    cout << "Private A: " << a.privateA << endl;
+    cout << "Protected A: " << a.protectedA << endl;
+
+    cout << "Accessing data from class B:" << endl;
+    cout << "Private B: " << b.privateB << endl;
+    cout << "Protected B: " << b.protectedB << endl;
+
+    cout << "Accessing data from class C:" << endl;
+    cout << "Private C: " << c.privateC << endl;
+    cout << "Protected C: " << c.protectedC << endl;
 }
 
 int main() {
-    // Create objects of A, B, and C
-    A objA(10, 20);
-    B objB(30, 40);
-    C objC(50, 60);
-
-    // Call the friend function to display data
-    displayData(objA, objB, objC);
-
+    
+    A a(1, 2);
+    B b(3, 4);
+    C c(5, 6);
+    accessData(a, b, c);
     return 0;
 }
